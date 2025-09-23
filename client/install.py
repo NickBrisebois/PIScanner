f8b6477382241543394e0dff87ded377e20da425
Nick Brisebois <email@nick-b.ca>
2025-09-23 19:36:17 +0100

    feat: install script to install as systemd service properly

---
+++ client/install.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import venv
+
+import argparse
+
+SERVICE_FILE = """
+[Unit]
+Description=PIScanner Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart={venv_dir}/bin/python3 -m piscanner_client
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"""
+
+def install_systemd_service(venv_dir: str):
+    with open("/etc/systemd/system/piscanner.service", "w") as f:
+        _ = f.write(SERVICE_FILE.format(venv_dir=venv_dir))
+
+    _ = os.system("systemctl daemon-reload")
+    _ = os.system("systemctl enable piscanner.service")
+    _ = os.system("systemctl start piscanner.service")
+
+
+def build_and_install(venv_dir: str):
+    print("Creating virtual environment at", venv_dir)
+    venv.create(venv_dir, with_pip=True)
+
+    print("Building...")
+    _ = os.system(f"{venv_dir}/bin/python -m build")
+
+    print("Installing...")
+    _ = os.system(f"{venv_dir}/bin/pip install dist/*.whl")
+
+
+if __name__ == "__main__":
+    if os.geteuid() != 0:
+        print("This script must be run as root.")
+        sys.exit(1)
+
+    if not os.path.exists("/etc/systemd/system"):
+        print("piscanner_client requires systemd")
+        sys.exit(1)
+
+    parser = argparse.ArgumentParser(description="Install PIScanner client")
+    _ = parser.add_argument("--install-dir", type=str, default="/opt/piscanner_client", help="Where to install the PIScanner client script")
+    args = parser.parse_args()
+
+    install_dir: str = args.install_dir
+    venv_dir = os.path.join(install_dir, "venv")
+    build_and_install(venv_dir=venv_dir)
+
+    install_systemd_service(venv_dir=venv_dir)
--- client/piscanner_client/hardware/webcam.py
+++ client/piscanner_client/hardware/webcam.py
@@ -9,25 +9,26 @@ class WebcamHandlerException(Exception):
 
 class WebcamController:
     _cam: cv2.VideoCapture
-    _capture_dir: str
+    _capture_base_dir: str
 
     def __init__(self, capture_dir: str = "/tmp/piscanner/captures") -> None:
         self._cam = cv2.VideoCapture(0)
         _ = self._cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         _ = self._cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-        self._capture_dir = capture_dir
-        os.makedirs(self._capture_dir, exist_ok=True)
+        self._capture_base_dir = capture_dir
+        os.makedirs(self._capture_base_dir, exist_ok=True)
 
 
     def capture_image(self, capture_dir: str):
+        capture_dir = os.path.join(self._capture_base_dir, capture_dir)
         os.makedirs(capture_dir, exist_ok=True)
 
         ret, frame = self._cam.read()
         if not ret:
             raise WebcamHandlerException("Failed to capture image")
 
-        file_name = f"{self._capture_dir}/{capture_dir}/cap_{int(time.time())}.jpg"
+        file_name = f"{capture_dir}/cap_{int(time.time())}.jpg"
         _ = cv2.imwrite(file_name, frame)
 
         print(f"Image saved to {file_name}")
